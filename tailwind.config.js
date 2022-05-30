3b0a096c9937ab04d1970fdf3e6c7000cc44cbae
Shafiq Hammad <shafiqhammad5@gmail.com>
2022-05-30 18:06:27 +0600

    ui improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
       center: true,
       padding: "16px",
     },
+    screens: {
+      sm: "540px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1140px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1320px",
+      // => @media (min-width: 1400px) { ... }
+    },
     fontFamily: {
       heading: "'Lexend', sans-serif",
       body: "'Inter', sans-serif",
@@ -18,6 +34,9 @@ module.exports = {
         primary: "#4A6CF7",
         "dark-text": "#79808A",
         dark: "#111722",
+      },
+      dropShadow: {
+        light: "drop-shadow(0px 1px 5px rgba(0, 0, 0, 0.1))",
       },
     },
   },
