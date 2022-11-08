47af4abd88d3c799feab09da837d953efad1f0e4
Juhan-Ahamed <juhan669@gmail.com>
2022-11-08 11:34:34 +0600

    gulp to webpack conversion done

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -58,9 +58,12 @@ document.addEventListener("DOMContentLoaded", init);
 
 // Init App
 function init() {
-  const txtElement = document.querySelector(".txt-type");
-  const words = JSON.parse(txtElement.getAttribute("data-words"));
-  const wait = txtElement.getAttribute("data-wait");
-  // Init TypeWriter
-  new TypeWriter(txtElement, words, wait);
+  const txtElement = document.querySelectorAll(".txt-type");
+  if(txtElement.length) {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute("data-words"));
+    const wait = txtElement.getAttribute("data-wait");
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+  }
 }
