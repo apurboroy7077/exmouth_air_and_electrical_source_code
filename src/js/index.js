460dd011286e3347c6dc7c0280eb8ed6d821e09f
Juhan-Ahamed <juhan669@gmail.com>
2023-04-06 16:22:16 +0600

    update package

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,24 @@
-import '../../node_modules/tiny-slider/dist/tiny-slider.css';
-import '../css/animate.css';
-import '../css/style.css';
+import '../../node_modules/tiny-slider/dist/tiny-slider.css'
+import '../css/animate.css'
+import '../css/style.css'
 
-import '../js/typewriter';
-import { tns } from 'tiny-slider';
-import Isotope from 'isotope-layout';
-import WOW from 'wowjs';
+import Isotope from 'isotope-layout'
+import { tns } from 'tiny-slider'
+import WOW from 'wowjs'
+import '../js/typewriter'
 
 window.wow = new WOW.WOW({
   live: false,
-});
+})
 
 window.wow.init({
   offset: 50,
-});
+})
 
 //========= testimonial
-const testimonial = document.querySelectorAll('.testimonial-active');
+const testimonial = document.querySelectorAll('.testimonial-active')
 
-if(testimonial.length) {
+if (testimonial.length) {
   tns({
     container: '.testimonial-active',
     items: 1,
@@ -51,12 +51,12 @@ if(testimonial.length) {
         />
       </svg>`,
     ],
-  });
+  })
 }
 
 //============== isotope masonry js
 
-const elem = document.querySelector('.portfolio-grid');
+const elem = document.querySelector('.portfolio-grid')
 
 const iso = new Isotope(elem, {
   // options
@@ -65,137 +65,135 @@ const iso = new Isotope(elem, {
     // use outer width of grid-sizer for columnWidth
     columnWidth: '.grid-sizer',
   },
-});
+})
 
-const filterButtons = document.querySelectorAll('.portfolio-btn-wrapper button');
+const filterButtons = document.querySelectorAll('.portfolio-btn-wrapper button')
 filterButtons.forEach((e) =>
   e.addEventListener('click', () => {
-    const filterValue = event.target.getAttribute('data-filter');
+    const filterValue = event.target.getAttribute('data-filter')
     iso.arrange({
       filter: filterValue,
-    });
+    })
   })
-);
+)
 
 //======= portfolio-btn active
-const elements = document.querySelectorAll('.portfolio-btn-wrapper button');
+const elements = document.querySelectorAll('.portfolio-btn-wrapper button')
 for (let i = 0; i < elements.length; i++) {
   elements[i].onclick = function () {
     // remove class from sibling
 
-    let el = elements[0];
+    let el = elements[0]
     while (el) {
       if (el.tagName === 'BUTTON') {
         //remove class
-        el.classList.remove('active');
+        el.classList.remove('active')
       }
       // pass to the new sibling
-      el = el.nextSibling;
+      el = el.nextSibling
     }
 
-    this.classList.add('active');
-  };
+    this.classList.add('active')
+  }
 }
 
-
-
-(function () {
-  'use strict';
+;(function () {
+  'use strict'
 
   // ======= Sticky
   window.onscroll = function () {
-    const ud_header = document.querySelector('.header');
-    const sticky = ud_header.offsetTop;
+    const ud_header = document.querySelector('.header')
+    const sticky = ud_header.offsetTop
 
     if (window.pageYOffset > sticky) {
-      ud_header.classList.add('sticky');
+      ud_header.classList.add('sticky')
     } else {
-      ud_header.classList.remove('sticky');
+      ud_header.classList.remove('sticky')
     }
 
     // show or hide the back-top-top button
-    const backToTop = document.querySelector('.back-to-top');
+    const backToTop = document.querySelector('.back-to-top')
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      backToTop.style.display = 'flex';
+      backToTop.style.display = 'flex'
     } else {
-      backToTop.style.display = 'none';
+      backToTop.style.display = 'none'
     }
-  };
+  }
 
   // Mobile Menu
-  const menuToggler = document.querySelector('.menu-toggler');
-  const menuWrapper = document.querySelector('.menu-wrapper');
+  const menuToggler = document.querySelector('.menu-toggler')
+  const menuWrapper = document.querySelector('.menu-wrapper')
 
   menuToggler.addEventListener('click', () => {
-    menuWrapper.classList.toggle('show');
-    document.body.classList.toggle('overflow-y-hidden');
-    menuToggler.querySelector('.cross').classList.toggle('hidden');
-    menuToggler.querySelector('.menu').classList.toggle('hidden');
-  });
+    menuWrapper.classList.toggle('show')
+    document.body.classList.toggle('overflow-y-hidden')
+    menuToggler.querySelector('.cross').classList.toggle('hidden')
+    menuToggler.querySelector('.menu').classList.toggle('hidden')
+  })
 
   //===== close navbar-collapse when a  clicked
   document.querySelectorAll('.navbar li:not(.submenu-item) a').forEach((e) =>
     e.addEventListener('click', () => {
-      menuWrapper.classList.toggle('show');
-      document.body.classList.toggle('overflow-y-hidden');
-      menuToggler.querySelector('.cross').classList.toggle('hidden');
-      menuToggler.querySelector('.menu').classList.toggle('hidden');
+      menuWrapper.classList.toggle('show')
+      document.body.classList.toggle('overflow-y-hidden')
+      menuToggler.querySelector('.cross').classList.toggle('hidden')
+      menuToggler.querySelector('.menu').classList.toggle('hidden')
     })
-  );
+  )
 
   // ===== Sub-menu
-  const submenuItems = document.querySelectorAll('.submenu-item');
+  const submenuItems = document.querySelectorAll('.submenu-item')
   submenuItems.forEach((el) => {
     el.querySelector('a').addEventListener('click', () => {
-      el.querySelector('a').classList.toggle('active');
-      el.querySelector('.submenu').classList.toggle('hidden');
-    });
-  });
+      el.querySelector('a').classList.toggle('active')
+      el.querySelector('.submenu').classList.toggle('hidden')
+    })
+  })
 
   // ====== scroll top js
   function scrollTo(element, to = 0, duration = 500) {
-    const start = element.scrollTop;
-    const change = to - start;
-    const increment = 20;
-    let currentTime = 0;
+    const start = element.scrollTop
+    const change = to - start
+    const increment = 20
+    let currentTime = 0
 
     const animateScroll = () => {
-      currentTime += increment;
+      currentTime += increment
 
-      const val = Math.easeInOutQuad(currentTime, start, change, duration);
+      const val = Math.easeInOutQuad(currentTime, start, change, duration)
 
-      element.scrollTop = val;
+      element.scrollTop = val
 
       if (currentTime < duration) {
-        setTimeout(animateScroll, increment);
+        setTimeout(animateScroll, increment)
       }
-    };
+    }
 
-    animateScroll();
+    animateScroll()
   }
 
   Math.easeInOutQuad = function (t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  };
+    t /= d / 2
+    if (t < 1) return (c / 2) * t * t + b
+    t--
+    return (-c / 2) * (t * (t - 2) - 1) + b
+  }
 
   document.querySelector('.back-to-top').onclick = () => {
-    scrollTo(document.documentElement);
-  };
-})();
+    scrollTo(document.documentElement)
+  }
+})()
 
 // Document Loaded
 document.addEventListener('DOMContentLoaded', () => {
   // ==== darkToggler
-  const darkTogglerCheckbox = document.querySelector('#darkToggler');
-  const html = document.querySelector('html');
+  const darkTogglerCheckbox = document.querySelector('#darkToggler')
+  const html = document.querySelector('html')
 
   const darkModeToggler = () => {
-    darkTogglerCheckbox.checked ? html.classList.remove('dark') : html.classList.add('dark');
-  };
-  darkModeToggler();
+    darkTogglerCheckbox.checked ? html.classList.remove('dark') : html.classList.add('dark')
+  }
+  darkModeToggler()
 
-  darkTogglerCheckbox.addEventListener('click', darkModeToggler);
-});
+  darkTogglerCheckbox.addEventListener('click', darkModeToggler)
+})
